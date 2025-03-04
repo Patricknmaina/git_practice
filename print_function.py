d8cf65bcc34371f4a09a856335e821a3ee2b9511
Patrick Maina <patrickmaina364@gmail.com>
2025-03-04 23:56:58 +0300

    added custom print function

---
+++ print_function.py
@@ -0,0 +1,8 @@
+# Custom print function
+
+def print_function():
+	custom_dict = {'Name': 'Patrick', 'Age': 25, 'Profession': 'Electronics Engineer', 'Email': 'patrick123@gmail.com'}
+	return custom_dict
+
+data = print_function()
+print(data)
